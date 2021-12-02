30914e5ccd84b8fd87385c9ff1dc41fc017800c9
gabor <g.bozsik@gmail.com>
2021-12-02 20:22:45 +0100

    It works with webcam.

--- face_detector.py
+++ face_detector.py
@@ -1,30 +1,38 @@
 import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# img = cv2.imread('rossi_hamy.jpg')
+#
+# img = cv2.imread('rossi.jpg')
 # grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
+#
 # for rectangle in face_coordinates:
 #     (x, y, width, height) = rectangle
 #     cv2.rectangle(img, (x, y), (x+width, y+height), (0, 255, 0), 2)
 
+# print(face_coordinates)
+
+# cv2.imshow('Rossi: ', img)
+# cv2.waitKey()
 
 
 webcam = cv2.VideoCapture(0)
 while True:
     successful_frame, frame = webcam.read()
-    grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
-    cv2.imshow('face detector', grayscale_img)
-    cv2.waitKey(1)
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    for (x, y, width, height) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    cv2.imshow('face detector', frame)
+    key = cv2.waitKey(1)
+
+    if key == 81 or key == 113:
+        break
+
+webcam.release()
 
 
-print(face_coordinates)
-
-cv2.imshow('Rossi: ', img)
-cv2.waitKey()
 
 
 
-print('hali')
